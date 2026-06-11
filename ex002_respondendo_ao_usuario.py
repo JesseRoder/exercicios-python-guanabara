b74ffd776f161f7915ea2ad926eeaab0aac988b1
Jesse Roder <gesseroder@gmail.com>
2026-06-11 17:04:14 +0100

    exercicio 003 - respondendo ao usuario

--- ex002_respondendo_ao_usuario.py
+++ ex002_respondendo_ao_usuario.py
@@ -1,4 +1,4 @@
-# Faça um programa que leia o nome do usuário e mostre uma mensagem de boas-vindas.
+# DESAFIO 002: Faça um programa que leia o nome do usuário e mostre uma mensagem de boas-vindas.
 
 nome = input("Digite seu nome: ")
 print(f"Olá, {nome}! É um prazer te conhecer!")
---
+++ ex003_somando_dois_numeros.py
@@ -0,0 +1,6 @@
+# DESAFIO 003:Faça um programa que leia dois números inteiros e mostre a soma entre eles.
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+soma = n1 + n2
+print(f"{n1} + {n2} = {soma}")
