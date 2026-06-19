cca810cc6660d7f5cd7c4c4c42c49249f1a7e2ec
Jesse Roder <gesseroder@gmail.com>
2026-06-19 12:12:36 +0100

    exercicio 005 - antecessor e sucessor

---
+++ ex005_antecessor_sucessor.py
@@ -0,0 +1,4 @@
+# DESAFIO 005 - Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+num = int(input("Digite um número inteiro: "))
+print(f"O número digitado foi {num}, seu antecessor é {num - 1} e seu sucessor é {num + 1}.")
