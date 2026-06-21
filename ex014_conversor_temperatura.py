4474c676dfb7fd20c9d38c2643cf51535557555f
Jesse Roder <gesseroder@gmail.com>
2026-06-21 13:33:44 +0100

    exercicio 014 - conversor de temperaturas

---
+++ ex014_conversor_temperatura.py
@@ -0,0 +1,5 @@
+# DESAFIO 014 - Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"A temperatura em graus Fahrenheit é: {fahrenheit:.2f}°F")
